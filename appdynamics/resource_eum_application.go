6eab29fc98c468149504b82e0fde2e185d257cf6
Michael Maurer <mimaurer@cisco.com>
2021-02-03 17:10:46 +0100

    improved eum read

--- appdynamics/resource_eum_application.go
+++ appdynamics/resource_eum_application.go
@@ -116,11 +116,19 @@ func resourceEUMApplicationRead(ctx context.Context, d *schema.ResourceData, m i
 	data := Entries{}
 	_ = json.Unmarshal([]byte(body), &data)
 
+
 	for i := 0; i < len(data); i++ {
-		if (data[i].Name == d.Get("name").(string)) {
-			d.SetId(fmt.Sprint(data[i].ID))
-			d.Set("name", data[i].Name)
-			d.Set("eum_key", data[i].AppKey)
+		if (d.Id() == "") {
+			if (data[i].Name == d.Get("name").(string)) {
+				d.SetId(fmt.Sprint(data[i].ID))
+				d.Set("name", data[i].Name)
+				d.Set("eum_key", data[i].AppKey)
+			}
+		} else {
+			if (fmt.Sprint(data[i].ID) == d.Id()) {
+				d.Set("name", data[i].Name)
+				d.Set("eum_key", data[i].AppKey)
+			}
 		}
 	}
 
